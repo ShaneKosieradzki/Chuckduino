c8c029c13a1a77c29de0f082ec6a61f75ba2be8c
kosieram <mitchkos21@gmail.com>
2020-08-18 19:39:27 -0500

    simpler frame definition (assumes grayscale)

--- include/Chuckduino.h
+++ include/Chuckduino.h
@@ -19,8 +19,7 @@ public:
     struct Precepts {
         uint16_t width;
         uint16_t height;
-        uint16_t depth;
-        uint8_t*** frame;
+        uint8_t** frame;
     };
 
     struct Actions {
